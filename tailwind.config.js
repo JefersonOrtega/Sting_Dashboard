aead8b8ea307b4f38822b7a3ee00060a59256ec0
MartinsitoBritoDiaz <martinsitobd@gmail.com>
2023-05-23 18:58:23 -0400

    feat: font and color styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        dark: "#212A3E",
+        light: "#F1F6F9",
+        "light-text": "#ffffff",
+        blue: "#394867",
+        gray: "#9BA4B5",
+        // blue: "#3d403a",
+        // "dark-blue": "#012e40",
+        // "dark-gray": "#f0f3f4 ",
+      },
+      fontFamily: {
+        popins: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        sm: { max: "800px" },
+        md: { min: "800px" },
+        lg: { min: "1280px" },
+        // => @media (max-width: 639px) { ... }
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
