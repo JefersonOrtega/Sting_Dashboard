6c7508645f589237e2e5557bbd00a5978feaee2e
MartinsitoBritoDiaz <martinsitobd@gmail.com>
2023-05-26 18:13:51 -0400

    feat: structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../styles/globals.css";
 
 export const metadata = {
   title: "Sting",
@@ -12,7 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {children}
+        </main>
+      </body>
     </html>
   );
 }
